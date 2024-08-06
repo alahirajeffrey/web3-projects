3211137683a5ec463f4bb8ed3d081ac6daf1b316
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-06 16:49:12 +0100

    created function to create files if they do not exists

--- eth-wallets/src/utils.js
+++ eth-wallets/src/utils.js
@@ -1,5 +1,25 @@
+import path from "path"
+import fs from "fs"
+
 export const createAccount = () => {}
 
 export const transferEth = () => {}
 
 export const getAccountDetails = () => {}
+
+/**
+ * check if a file exists and create if it does not
+ * @param { string } filePath : path of file to be created
+ * @param { array } initialData : initial data to include
+ * @returns void
+ */
+export const checkAndCreateFIle = (filePath, initialData) => {
+    if(!fs.existsSync(filePath)){
+        fs.writeFileSync(filePath, JSON.stringify(initialData))
+        console.log(`${path.basename(filePath)} created`)
+        return
+    }else{
+        console.log(`${path.basename(filePath)} already exists`)
+        return
+    }
+}
