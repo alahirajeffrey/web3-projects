61a9508a2a45bf5001400e2f99e9ee5e6eaa7fac
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-06 16:50:01 +0100

    check if accounts.json and transactions.json exist

--- eth-wallets/src/index.js
+++ eth-wallets/src/index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+import path, { dirname } from "path"
+import { checkAndCreateFIle } from "./utils.js"
+
+const accountFilePath = path.join(dirname("./"), "accounts.json")
+const transactionFilePath = path.join(dirname("./"), "transactions.json")
+
+// create files if they do not exists
+checkAndCreateFIle(accountFilePath, [])
+checkAndCreateFIle(transactionFilePath, [])
